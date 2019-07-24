686e5c524909331fcbe51ad5e2be94a21b1905f0
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2019-07-24 13:14:49 -0300

    test

--- filter_obs.R
+++ filter_obs.R
@@ -4,9 +4,9 @@ library(data.table)
 source('read_files.R')
 
 # Quiero un archivo para cada tipo de observacion 
-var <- 83073
-path_in <- "/home/paola.corrales/datosmunin/RRA_Obs/asimiladas/obs_2018111*_*_asimiladas.dat"
+var <- NULL
+path_in <- "/home/paola.corrales/datosmunin/RRA_Obs/asimiladas/obs_20181120_*_asimiladas.dat"
 path_out <- paste0("/home/paola.corrales/datosmunin/RRA_Obs/filtradas/obs_", var, ".csv")
 
-obs <- read.obs.asim(path_in, keep.obs = c(var), keep.time.slot = c(1,7))
+obs <- read.obs.asim(path_in, keep.obs = c(83073), keep.time.slot = c(7))
 fwrite(obs, path_out)
