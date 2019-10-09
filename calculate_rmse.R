58c568965a85b7d5e89eaae4b6997925c6ea8baa
paocorrales <paobcorrales@gmail.com>
2019-10-09 14:46:42 -0300

    Indentación, por favor, gracias.

--- calculate_rmse.R
+++ calculate_rmse.R
@@ -15,16 +15,16 @@ out <- foreach(f = 1:length(files),
                .packages = c("data.table", "lubridate", "dplyr"),
                .export = c("files"),
                .combine = "rbind") %dopar% {
-
-cat("Leyendo el pronóstico ", basename(files[f]), "\n")
-fcst <- fread(files[f])
-
-fecha_ini <- ymd_h(stringr::str_extract(files[f], "\\d{8}_\\d{2}"))
-
-temp <- fcst[, `:=`(obs.fcst = obs - fcst,
-           fecha.ini = fecha_ini,
-           verif = as.numeric(as.duration(as_datetime(time.obs) - fecha_ini), "hour"))] %>%
-  .[, .(rmse = sqrt(mean(obs.fcst^2)), bias = mean(obs.fcst)), by = .(ens, verif, fecha.ini)]
-}
+                 
+                 cat("Leyendo el pronóstico ", basename(files[f]), "\n")
+                 fcst <- fread(files[f])
+                 
+                 fecha_ini <- ymd_h(stringr::str_extract(files[f], "\\d{8}_\\d{2}"))
+                 
+                 temp <- fcst[, `:=`(obs.fcst = obs - fcst,
+                                     fecha.ini = fecha_ini,
+                                     verif = as.numeric(as.duration(as_datetime(time.obs) - fecha_ini), "hour"))] %>%
+                   .[, .(rmse = sqrt(mean(obs.fcst^2)), bias = mean(obs.fcst)), by = .(ens, verif, fecha.ini)]
+               }
 
 fwrite(out, "/home/paola.corrales/datosmunin/RRA_Fcst/estadisticos/rmse_83073.csv")
